505fac8537007c0c1f5843c8843b6b987c702ef9
unknown <teja.saiteja3@gmail.com>
2021-07-30 20:15:37 +0530

    added trukea parent,trukea customer,accountservice

---
+++ TrukeaCustomer/src/main/java/com/trukea/customer/TrukeaCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.trukea.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrukeaCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrukeaCustomerApplication.class, args);
+	}
+
+}
