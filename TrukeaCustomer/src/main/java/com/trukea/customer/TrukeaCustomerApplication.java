072b5f47162527fdcd800321f21efbdf5f1e8c38
unknown <teja.saiteja3@gmail.com>
2021-07-30 21:42:01 +0530

    modified gitignore

--- TrukeaCustomer/src/main/java/com/trukea/customer/TrukeaCustomerApplication.java
+++ TrukeaCustomer/src/main/java/com/trukea/customer/TrukeaCustomerApplication.java
@@ -1,13 +1,29 @@
 package com.trukea.customer;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
-public class TrukeaCustomerApplication {
+
+public class TrukeaCustomerApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrukeaCustomerApplication.class, args);
 	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/trukeahome").setViewName("Trukea-Home");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/register").setViewName("register");
+	}
+
+	
+	
+	
 
 }
